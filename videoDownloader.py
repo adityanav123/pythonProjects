16a922e9eb1fdf65741c3d0cb41e1636ed3ce6ac
Aditya Navphule <anavphule@gmail.com>
2019-02-28 21:00:04 +0530

    showing video size while downloading

--- videoDownloader.py
+++ videoDownloader.py
@@ -1,10 +1,23 @@
+import math
+
+def convert_size(size_bytes):
+   if size_bytes == 0:
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_name[i])
+
 from pytube import YouTube
 print('Enter a link : ')
 link = input()
 yt = YouTube(link)
 
 print('Video Title : ', yt.title)
-
 stream = yt.streams.first()
+print("Stream selected : " , stream)
+print('Video Size : ', convert_size(stream.filesize))
+
 
 stream.download()
